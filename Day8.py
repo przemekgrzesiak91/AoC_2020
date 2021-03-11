4902f4b517dc51a0c52fdfd7c07c983f4f2063d8
Przemyslaw Grzesiak <przemek.grzesiak91@gmail.com>
2021-03-11 07:17:21 +0100

    Add Day 8 solution

--- Day8.py
+++ Day8.py
@@ -1,22 +1,42 @@
+import copy
 data = [list(line.strip().split(' ')) for line in open('data/day8.txt')]
 
-accumulator = 0
-current = 0
-visited = []
 
-while current not in visited:
-    visited.append(current)
+def part1(data):
 
-    if data[current][0] == 'acc':
-        accumulator += int(data[current][1])
-        current += 1
+    accumulator = 0
+    current = 0
+    visited = []
 
-    elif data[current][0] == 'nop':
-        current += 1
+    while current not in visited:
+        visited.append(current)
 
-    elif data[current][0] == 'jmp':
-        current += int(data[current][1])
+        if data[current][0] == 'acc':
+            accumulator += int(data[current][1])
+            current += 1
 
-print("Day 8.1: ", accumulator)
+        elif data[current][0] == 'nop':
+            current += 1
+
+        elif data[current][0] == 'jmp':
+            current += int(data[current][1])
+
+        if current >= len(data):
+            return(1,accumulator)
+
+    return(0,accumulator)
+
+print("Day 8.1: ", part1(data)[1])
+
+def part2(data):
+    acc_list =[]
+    for i in range(0,len(data)):
+        new_data = copy.deepcopy(data)
+        if data[i][0] == 'nop': new_data[i][0] = 'jmp'
+        elif data[i][0] == 'jmp': new_data[i][0] = 'nop'
+
+        result = part1(new_data)
+        if result[0] == 1: return result[1]
 
 
+print("Day 8.2: ", part2(data))
